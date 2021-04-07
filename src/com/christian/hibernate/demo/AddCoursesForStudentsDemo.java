9908653270170868f33d17b4a5515692859d7eb8
Christian <christian@sanglech.com>
2021-04-07 22:04:03 +0900

    Created add courses for students

---
+++ src/com/christian/hibernate/demo/AddCoursesForStudentsDemo.java
@@ -0,0 +1,68 @@
+package com.christian.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.christian.hibernate.demo.entity.Course;
+import com.christian.hibernate.demo.entity.Instructor;
+import com.christian.hibernate.demo.entity.InstructorDetail;
+import com.christian.hibernate.demo.entity.Review;
+import com.christian.hibernate.demo.entity.Student;
+
+public class AddCoursesForStudentsDemo {
+
+	public static void main(String[] args) {
+		
+		///create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.addAnnotatedClass(Review.class)
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//create session
+		
+		Session session= factory.getCurrentSession();
+		
+		try {
+			
+			//start txn
+			session.beginTransaction();
+			
+			//get the student from db
+			
+			int studentId=2;
+			
+			Student tempStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Student working with: "+ tempStudent);
+			//create more courses
+			
+			Course tempCourse1= new Course("How to Game");
+			Course tempCourse2= new Course("How to build a house");
+			
+			//add student to course
+			tempCourse1.addStudent(tempStudent);
+			tempCourse2.addStudent(tempStudent);
+			
+			//save the courses
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+			
+			
+			//comit txn
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+		}finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
